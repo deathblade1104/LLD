cddb06026b6cdab45d52a237a0ef9048e34b464d
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2025-10-03 20:54:33 +0530

    Factory Design Pattern Done

---
+++ design-patterns/creational/factory/Main.java
@@ -0,0 +1,68 @@
+
+abstract class Vehicle {
+  protected VehicleType type;
+
+  public VehicleType getType() {
+    return type;
+  }
+}
+
+// ✅ Concrete products
+class Car extends Vehicle {
+  public Car() {
+    this.type = VehicleType.Car;
+  }
+}
+
+class Bike extends Vehicle {
+  public Bike() {
+    this.type = VehicleType.Bike;
+  }
+}
+
+class Truck extends Vehicle {
+  public Truck() {
+    this.type = VehicleType.Truck;
+  }
+}
+
+abstract class VehicleFactory {
+  abstract Vehicle createVehicle();
+}
+
+class CarFactory extends VehicleFactory {
+  @Override
+  public Vehicle createVehicle() {
+    return new Car();
+  }
+}
+
+class BikeFactory extends VehicleFactory {
+  @Override
+  public Vehicle createVehicle() {
+    return new Bike();
+  }
+}
+
+class TruckFactory extends VehicleFactory {
+  @Override
+  public Vehicle createVehicle() {
+    return new Truck();
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    VehicleFactory carFactory = new CarFactory();
+    VehicleFactory bikeFactory = new BikeFactory();
+    VehicleFactory truckFactory = new TruckFactory();
+
+    Vehicle car = carFactory.createVehicle();
+    Vehicle bike = bikeFactory.createVehicle();
+    Vehicle truck = truckFactory.createVehicle();
+
+    System.out.println("Created vehicle: " + car.getType());
+    System.out.println("Created vehicle: " + bike.getType());
+    System.out.println("Created vehicle: " + truck.getType());
+  }
+}
